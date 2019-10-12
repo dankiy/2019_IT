bb9a7a8bd1d5c747d04ed79c5c8e9d65f9a4496b
Danil Kulminskiy <iorsadg@gmail.com>
2019-10-12 15:20:31 +0300

    333_Kulminskiy_Danil

--- task3.py
+++ task3.py
@@ -1,23 +1,11 @@
 import numpy as np
 
 def get_indices(N, n_batches, split_ratio):
-    """Generates splits of indices from 0 to N-1 into uniformly distributed\
-       batches. Each batch is defined by 3 indices [i, j, k] where\
-       (j-i) = split_ratio*(k-j). The first batch starts with i = 0,\
-       the last one ends with k = N - 1.
-    Args:
-        N (int): total counts
-        n_batches (int): number of splits
-        split_ratio (float): split ratio, defines position of j in [i, j, k].
-
-    Returns:
-        generator for batch indices [i, j, k]
-    """
-    inds = np.array([0, 0, 0])
+    j = ((N-1) / (n_batches*split_ratio+1))
+    inds = np.array([0, j, (j*(1+split_ratio))])
     for i in range(n_batches):
-        # todo: move forward batch
-        # calculate new indices
-        yield inds
+        yield np.array(inds, dtype=int)
+        inds += [(j*split_ratio)]*3
 
 def main():
     for inds in get_indices(100, 5, 0.25):
